9e2e080b721f9b83f32f1a324aa9afc49f6de7b9
ZuMuZa <zanova.zz@gmail.com>
2023-04-04 16:32:29 +0200

    Ukol_1

---
+++ ukol_1.py
@@ -0,0 +1,11 @@
+jmeno = "zuzka"
+mail = jmeno + "@czechitas.cz"
+print(mail)
+
+
+jmeno_a_prijmeni = input("Zadej své jméno a příjmení: ")
+
+print(jmeno_a_prijmeni.upper())
+print(jmeno_a_prijmeni.lower())
+
+# Děkuji za kontrolu :-) Stihla jsem jen pár věcí, snad bude vše ok. Přeji hezký den Zuzka Musilová :-)
